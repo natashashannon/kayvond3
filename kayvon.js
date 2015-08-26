6c6b1e462e2dfefb839d22a126decf372afa1eb9
Natasha Crowley <natashashannon@gmail.com>
2015-08-25 21:59:54 -0700

    d3 bar part 2

--- kayvon.js
+++ kayvon.js
@@ -1,24 +1,44 @@
 window.onload=function(){
-	var data = [4, 8, 15, 16, 23, 42];
 
-d3.select("body")
-    .style("color", "black")
-    .style("background-color", "pink");
+	d3.select("body")
+	    .style("color", "black")
+	    .style("background-color", "aqua");
 
-    var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+	// simple bar chart http://bost.ocks.org/mike/bar/2/
+	var width = 420,
+	    barHeight = 20;
 
+	var x = d3.scale.linear()
+	    .range([0, width]);
 
-// simple bar chart http://bost.ocks.org/mike/bar/2/
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+	var chart = d3.select(".chart")
+	    .attr("width", width);
 
+	d3.tsv("data.tsv", type, function(error, data) {
+		x.domain([0, d3.max(data, function(d) { return d.value; })]);
 
+		chart.attr("height", barHeight * data.length);
+
+		var bar = chart.selectAll("g")
+		    .data(data)
+		  .enter().append("g")
+		    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+		bar.append("rect")
+		    .attr("width", function(d) { return x(d.value); })
+		    .attr("height", barHeight - 1);
+
+		bar.append("text")
+		    .attr("x", function(d) { return x(d.value) - 3; })
+		    .attr("y", barHeight / 2)
+		    .attr("dy", ".35em")
+		    .text(function(d) { return d.value; });
+
+	});
+	function type(d) {
+		d.value = +d.value;
+		return d;
+	}
 }
 
 
